726184fac5f0390fea5fa9e4d946a6fbe5c65b75
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-03-11 16:14:41 +0100

    not working co

--- app/src/app/api/login/route.js
+++ app/src/app/api/login/route.js
@@ -1,9 +1,29 @@
 import { NextResponse } from "next/server";
 // Handles GET requests to /api
+const Sequelize = require("sequelize");
+
+async function getSequelizeConnection() {
+  const sequelize = new Sequelize("pomme_d_api", "admin", "password", {
+    host: "localhost",
+    dialect: "mysql",
+  });
+
+  // Tester la connexion
+  try {
+    await sequelize.authenticate();
+    console.log("Connexion à la base de données réussie !");
+  } catch (error) {
+    console.error("Échec de la connexion à la base de données :", error);
+    throw error;
+  }
+
+  return sequelize;
+}
 
 export async function GET(request) {
   try {
     const sequelize = await getSequelizeConnection();
+    console.log(sequelize);
     // Utiliser sequelize ici
     console.log("Connexion à la base de données réussie !");
     return NextResponse.json({ message: "Hello World" });
