fbe13d062456d1d712067289c233e5be6015bd5e
jules-jean-louis1 <jules.jean-louis@laplateforme.io>
2024-03-12 16:57:18 +0100

    feat: wip login function

--- app/src/app/(public)/login/page.jsx
+++ app/src/app/(public)/login/page.jsx
@@ -5,7 +5,74 @@ import { Label } from "@/components/ui/label";
 import { useEffect, useState, useRef } from "react";
 
 const SignupPage = () => {
-  return <></>;
+  const usernameRef = useRef(null);
+  const passwordRef = useRef(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: usernameRef.current.value,
+          password: passwordRef.current.value,
+        }),
+      });
+
+      const data = await response.json();
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    fetchData();
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-1/2 h-1/2">
+        <div className="border rounded-md p-4 border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800">
+          <h2 className="text-3xl font-bold pb-3">Connexion</h2>
+          <form method="POST" onSubmit={handleSubmit}>
+            <Label
+              className="text-gray-600 dark:text-gray-400 required"
+              htmlFor="Username"
+            >
+              Nom d'utilisateur / Email
+            </Label>
+            <Input
+              className="border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800"
+              type="text"
+              name="username"
+              placeholder="Nom d'utilisateur / Email"
+              ref={usernameRef}
+            />
+            <Label
+              className="text-gray-600 dark:text-gray-400 required"
+              htmlFor="Password"
+            >
+              Mot de passe
+            </Label>
+            <Input
+              className="border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800"
+              type="password"
+              name="password"
+              placeholder="Mot de passe"
+              ref={passwordRef}
+            />
+            <div className="pt-2">
+              <Button type="submit">Connexion</Button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SignupPage;
--- app/src/app/api/login/route.js
+++ app/src/app/api/login/route.js
@@ -1,4 +1,8 @@
 import { NextResponse } from "next/server";
+import { getSequelizeConnection } from "../db";
+import { defineUserModel } from "@/models/models";
+import bcrypt from "bcryptjs";
+import { Op } from "sequelize";
 
 export async function GET(request) {
   return NextResponse.json({ message: "Hello World" });
@@ -6,6 +10,46 @@ export async function GET(request) {
 
 // Handles POST requests to /api
 export async function POST(request) {
-  // ...
-  return NextResponse.json({ message: "Hello World" });
+  try {
+    const data = await request.json();
+    if (data.username === "" && data.password === "") {
+      return NextResponse.json({ message: "Missing fields" }, { status: 400 });
+    }
+    if (data.username === "" || data.password === "") {
+      return NextResponse.json({ message: "Missing fields" }, { status: 400 });
+    }
+
+    const sequelize = await getSequelizeConnection();
+    const User = defineUserModel(sequelize);
+
+    const user = await User.findOne({
+      where: {
+        [Op.or]: [{ username: data.username }, { email: data.username }],
+      },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "User does not exist" },
+        { status: 400 }
+      );
+    }
+
+    const passwordMatch = bcrypt.compareSync(data.password, user.password);
+
+    if (!passwordMatch) {
+      return NextResponse.json(
+        { message: "Incorrect password" },
+        { status: 400 }
+      );
+    }
+
+    return NextResponse.json({ message: "Login successful" }, { status: 200 });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Internal server error" },
+      { status: 500 }
+    );
+  }
 }
